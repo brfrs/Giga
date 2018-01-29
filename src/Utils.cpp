106bef7e18154085dba3927623c7691c957e5ae2
brfrs <brentfreder@gmail.com>
2018-01-28 21:04:26 -0800

    Add parenthesis at end of c_str calls

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -4,12 +4,12 @@ using std::ofstream;
 
 bool Giga::detail::directoryAccessible(const std::string& path) 
 {
-    return access(path.c_str, X_OK) == 0;
+    return access(path.c_str(), X_OK) == 0;
 }
 
 bool Giga::detail::fileAccessible(const std::string& path)
 {
-    return access(path.c_str, W_OK) == 0;
+    return access(path.c_str(), W_OK) == 0;
 }
 
 void Giga::detail::write(const std::string& path, const std::string& out)
